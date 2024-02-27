57f622361c15e4c988cfb40f51e63987a7866aff
vagner <seu.email@customer.com.br>
2024-02-27 15:07:31 -0300

    Configurando para transformar projeto em PWA

--- src/components/tiposVeiculos.vue
+++ src/components/tiposVeiculos.vue
@@ -2,15 +2,30 @@
     <div class="w-100px h-100px rounded-full border border-[#888] p-7px  
         md:w-[120px] md:h-[120px]"
     >
+        
         <button @click="emit('selecionar',props.tipo)"
             class="w-full h-full flex justify-center items-center rounded-full 
              p-15px hover:p-[17px] md:p-20px md:hover:p-[22px]"
             :class="props.tipo === props.selecionado ? ['bg-marrom_c'] : ['bg-marrom_e']"
         >
-            <img class="" :src="`../../public/imagens/${props.tipo}.png`" alt="">
+            <img v-if="props.tipo === 'caminhoes'"
+                class="" 
+                src="../assets/caminhoes.png" 
+                alt="img"
+            >
+            <img v-if="props.tipo === 'carros'"
+                class="" 
+                src="../assets/carros.png" 
+                alt="img"
+            >
+            <img v-if="props.tipo === 'motos'"
+                class=""    
+                src="../assets/motos.png"
+                alt="img"
+            >
         </button>
+
     </div>
-    
 </template>
 
 <script setup>
